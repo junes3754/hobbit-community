d9f7c979c4858709bc5ab9429a789ff431a360c4
junes3754 <junes3754@gmail.com>
2020-06-28 21:52:19 +0900

    로딩 바

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
+import { Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd, // 1
+  NavigationCancel, // 1
+  NavigationError, // 1
+} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -7,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  loading = false;
   showFiller = false;
   title = 'Hobbit-community';
   panelOpenState = false;
@@ -26,7 +33,31 @@ export class LayoutComponent implements OnInit {
     'board-form',
     'member-form',
   ];
-  constructor() {}
+  constructor(private router: Router) {
+    router.events.subscribe((event: RouterEvent) => {
+      //  this.refreshToken(event);
+      this.updateLoadingBar(event); // 4
+    });
+  }
+
+  // private refreshToken(event: RouterEvent): void {      // 로그인여부 체크 함수
+  //   if (event instanceof NavigationStart && this.authService.isLoggedIn()) {
+  //     this.authService.refresh().catch((response) => null);
+  //   }
+  // }
+  private updateLoadingBar(event: RouterEvent): void {
+    // 3
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (
+      event instanceof NavigationEnd ||
+      event instanceof NavigationCancel ||
+      event instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
 
   ngOnInit(): void {}
 }
